161e94fec8660b030f16aa60e817f0cdaa1eac10
Ghanashyam Bhat <ghanashyambhat6@gmail.com>
2024-02-03 12:20:38 +0530

    remote ganache server

--- ChainForge/routes.py
+++ ChainForge/routes.py
@@ -38,7 +38,7 @@ import os
 import requests
 
 
-w3 = Web3(Web3.HTTPProvider("HTTP://localhost:7545"))
+w3 = Web3(Web3.HTTPProvider("HTTP://172.16.64.202:7545"))
 assert True is w3.is_connected()
 # Load the contract bytecode and ABI from files
 with open("Chainforge/cbi.bin", "r") as f:
